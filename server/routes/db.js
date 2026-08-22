5dbd74e3b22c5a2cf47e8a818c4bf66759c99b75
Shourya Shinde <shindeshauryaanil@gmail.com>
2026-08-22 23:20:17 +0530

    Connect DB

---
+++ server/routes/db.js
@@ -0,0 +1,16 @@
+import express from "express"
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
+
+const db = new pg.Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT
+});
+
+db.connect();
+
+export default db;
